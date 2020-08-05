2aa99e9ffb85f9e4f94873b60ed91d2afd075900
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2020-08-05 12:41:23 -0500

    New candite's attributes
    New Candidate's attributes: Telephone and PsuchologicalTest.

--- src/object/candidate.py
+++ src/object/candidate.py
@@ -1,4 +1,6 @@
 from dao.flask_config import db, ma
+from object.candidate_telephone import CandidateTelephone, CandidateTelephoneSchema
+from object.candidate_psychologicaltest import CandidatePsychologicalTest, CandidatePsychologicalTestSchema
 
 class Candidate(db.Model):
     __table_args__ = {"schema": "evaluationroom"}
@@ -7,14 +9,27 @@ class Candidate(db.Model):
     idcandidato = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String())
     apellidopaterno = db.Column(db.String())
+    apellidomaterno = db.Column(db.String())
+    fechanacimiento = db.Column(db.DateTime)
+    correoelectronico = db.Column(db.String())
 
-    def __init__(self, nombre, apellidopaterno):
+    telephones = db.relationship('CandidateTelephone', lazy="dynamic", 
+                primaryjoin='and_(Candidate.idcandidato==CandidateTelephone.idcandidato)')
+    
+    psychologicaltests = db.relationship('CandidatePsychologicalTest', lazy="dynamic", 
+                primaryjoin='and_(Candidate.idcandidato==CandidatePsychologicalTest.idcandidato)')
+    
+    def __init__(self, nombre, apellidopaterno, apellidomaterno, fechanacimiento, correoelectronico):
         self.nombre = nombre
         self.apellidopaterno = apellidopaterno
-
-    def serializable(self):
-        return {'nombre': self.nombre}
+        self.apellidomaterno = apellidomaterno
+        self.fechanacimiento = fechanacimiento
+        self.correoelectronico = correoelectronico
 
 class CandidateSchema(ma.Schema):
     class Meta:
-        fields = ('idcandidato', 'nombre', 'apellidopaterno')
+        fields = ('idcandidato', 'nombre', 'apellidopaterno', 'apellidomaterno', 'fechanacimiento', 'correoelectronico', 
+            'telephones', 'psychologicaltests')
+
+    telephones = ma.Nested(CandidateTelephoneSchema, many=True)
+    psychologicaltests = ma.Nested(CandidatePsychologicalTestSchema, many=True)
---
+++ src/object/candidate_telephone.py
@@ -0,0 +1,25 @@
+from dao.flask_config import db, ma
+from object.telephone import Telephone, TelephoneSchema
+
+class CandidateTelephone(db.Model):
+    __table_args__ = {"schema": "evaluationroom"}
+    __tablename__ = 'candidatotelefono'
+    
+    idcandidato = db.Column(db.Integer, db.ForeignKey('evaluationroom.candidato.idcandidato'), primary_key=True)
+    idtelefono = db.Column(db.Integer, db.ForeignKey('evaluationroom.telefono.idtelefono'), primary_key=True)
+    numero = db.Column(db.String())
+
+    telephone = db.relationship('Telephone', 
+                primaryjoin='and_(Candidate.idcandidato==CandidateTelephone.idcandidato, '
+                'CandidateTelephone.idtelefono==Telephone.idtelefono)')
+    
+    def __init__(self, id_candidato, id_telephone, number=None):
+        self.idcandidato = id_candidato
+        self.idtelefono = id_telephone
+        self.numero = number
+
+class CandidateTelephoneSchema(ma.Schema):
+    class Meta:
+        fields = ('idcandidato', 'idtelefono', 'numero', 'telephone')
+
+    telephone = ma.Nested(TelephoneSchema)
