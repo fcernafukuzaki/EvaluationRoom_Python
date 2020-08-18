724cc1d354491f0cba0b4c0cc6a12d48a34a5b2f
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2020-08-18 17:01:20 -0500

    CORS

--- src/dao/flask_config.py
+++ src/dao/flask_config.py
@@ -4,6 +4,8 @@ from flask_marshmallow import Marshmallow
 from flask_restful import Api
 from os import environ
 
+from flask_cors import CORS
+
 DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
 DATABASE_URI = DATABASE_URI[1:len(DATABASE_URI)-1]
 
@@ -13,3 +15,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
+
+CORS(app)
