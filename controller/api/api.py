a29c7ead821dc10ed11eb84a89462b226be67ed2
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2021-11-12 02:51:56 -0500

    Update api.py

--- controller/api/api.py
+++ controller/api/api.py
@@ -23,8 +23,12 @@ class PsychologicalTestInterpretacionController(Resource):
                 response_body = {'mensaje':"OK"}
                 return get_response_body(code=200, message="OK", user_message="OK", body=response_body), 200
             if uid:
-                #token = request.headers['Authorization']
-                token = request.json['headers']['Authorization']
+                print(request.headers)
+                print(request.headers.items())
+                print(request.json)
+                print(request.json.items())
+                token = request.headers['Authorization']
+                #token = request.json['headers']['Authorization']
                 print(f"{token}")
                 flag, respuesta, codigo, _ = authorizer_service.validate_recruiter_identify(token, email)
                 print("{}|{}".format(flag,respuesta))
