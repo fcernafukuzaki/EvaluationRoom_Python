b830a3aff5abd08ecd2a52d65228691079ee102e
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2022-03-07 13:11:29 -0500

    Validate numero documento

--- controller/candidate_form/candidate_controller.py
+++ controller/candidate_form/candidate_controller.py
@@ -60,11 +60,12 @@ class CandidateController(Resource):
                     user_message = f'Ya existe un candidato con el mismo correo electrónico. Debe ingresar un email distinto.'
                     return get_response_body(code=409, message=message, user_message=user_message), 409
                 
-                _, code, _ = candidate_service.get_candidate_by_document(numerodocumentoidentidad)
-                if code == 200:
-                    message = f'Error relacionado a Número de documento.'
-                    user_message = f'Ya existe un candidato con el mismo número de documento de identidad. Debe ingresar uno distinto.'
-                    return get_response_body(code=409, message=message, user_message=user_message), 409
+                if numerodocumentoidentidad:
+                    _, code, _ = candidate_service.get_candidate_by_document(numerodocumentoidentidad)
+                    if code == 200:
+                        message = f'Error relacionado a Número de documento.'
+                        user_message = f'Ya existe un candidato con el mismo número de documento de identidad. Debe ingresar uno distinto.'
+                        return get_response_body(code=409, message=message, user_message=user_message), 409
 
                 result, code, message = candidate_service.add_candidate(idcandidato, nombre, apellidopaterno, apellidomaterno, iddocumentoidentidad, numerodocumentoidentidad, idestadocivil,
                             cantidadhijos, fechanacimiento, correoelectronico, idsexo, self_register, telefonos, direcciones)
