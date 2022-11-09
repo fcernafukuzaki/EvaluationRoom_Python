eb6a9d8f08626e0a3b0957a548e8e6c6d6dbd3a1
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2022-11-09 15:53:31 -0500

    Feature/soporte tecnico (#19)

---
+++ objects/soporte_tecnico/mensaje_procesoseleccion_candidato.py
@@ -0,0 +1,18 @@
+from configs.flask_config import db, ma
+
+class MensajeProcesoseleccionCandidato(db.Model):
+    __table_args__ = {"schema": "evaluationroom"}
+    __tablename__ = 'mensaje_procesoseleccion_candidato'
+    
+    id_mensaje = db.Column(db.Integer, primary_key=True)
+    tipo_mensaje = db.Column(db.String())
+    mensaje = db.Column(db.String())
+
+    def __init__(self, id_mensaje, tipo_mensaje, mensaje):
+        self.id_mensaje = id_mensaje
+        self.tipo_mensaje = tipo_mensaje
+        self.mensaje = mensaje
+    
+class MensajeProcesoseleccionCandidatoSchema(ma.Schema):
+    class Meta:
+        fields = ('id_mensaje', 'tipo_mensaje', 'mensaje')
---
+++ service/soporte_tecnico/soportetecnico_notificacion_service.py
@@ -0,0 +1,18 @@
+from configs.flask_config import db
+from sqlalchemy import func
+from objects.soporte_tecnico.soportetecnico_notificacion import SoporteTecnicoNotificacion
+import json
+
+class SoporteTecnicoNotificacionService():
+
+    def guardar_mensaje_error_candidato(self, correo_electronico, observacion, detalle=None):
+        try:
+            nuevo_mensaje_error_candidato = SoporteTecnicoNotificacion(correo_electronico, func.now(), observacion, json.dumps(detalle))
+
+            db.session.add(nuevo_mensaje_error_candidato)
+            db.session.commit()
+
+            return True, 'Registro exitoso.'
+        except:
+            print('Error al registrar notificación de error del candidato {}.'.format(correo_electronico))
+            return False, 'Error al registrar notificación de error.'
