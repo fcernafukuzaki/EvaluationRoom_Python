7801ee60d21ef7a7a6d6116ac87e64d0c14dfdf0
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2022-11-09 23:19:06 -0500

    Format

--- common/util.py
+++ common/util.py
@@ -2,7 +2,7 @@ import urllib3
 
 
 def str2bool(v):
-    '''Retorna un Boolean a partir de un palabra que se encuentre en la lista'''
+    """Retorna un Boolean a partir de un palabra que se encuentre en la lista"""
     return v.lower() in ("yes", "true", "t", "1")
 
 
@@ -12,16 +12,18 @@ def field_in_dict(dictionary, field):
     return False
 
 
-def get_response_body(code=200, message='OK', user_message='', body=None):
+def get_response_body(code=200, message="OK", user_message="", body=None):
     if code == 200:
-        return {'code':200, 'message':message, 'user_message':user_message, 'body':body}
-    return {'error':{'code':code, 'message':message, 'user_message':user_message}}
+        return {
+            "code": 200,
+            "message": message,
+            "user_message": user_message,
+            "body": body,
+        }
+    return {"error": {"code": code, "message": message, "user_message": user_message}}
 
 
-def invoke_api(url, body, headers={'Content-Type': 'application/json'}, method='POST'):
+def invoke_api(url, body, headers={"Content-Type": "application/json"}, method="POST"):
     http = urllib3.PoolManager()
-    response = http.request(method,
-                            url,
-                            body=body,
-                            headers=headers)
+    response = http.request(method, url, body=body, headers=headers)
     return response
--- configs/flask_api_config.py
+++ configs/flask_api_config.py
@@ -25,98 +25,98 @@ from controller.administrator.usuarios_controller import *
 from controller.administrator.perfiles_controller import *
 
 api.add_resource(LoginUserController, 
-    '/login/authenticate')
+    "/login/authenticate")
 
 api.add_resource(CandidateController, 
-    '/v1/candidate',
-    '/v1/candidate/self_registered=<string:self_registered>',
-    '/v1/candidate/uid=<int:uid>',
-    '/v1/candidate/email=<string:email>')
+    "/v1/candidate",
+    "/v1/candidate/self_registered=<string:self_registered>",
+    "/v1/candidate/uid=<int:uid>",
+    "/v1/candidate/email=<string:email>")
 
 api.add_resource(CandidateInfoSimpleController, 
-    '/v1/candidate_info')
+    "/v1/candidate_info")
 
 api.add_resource(SelectionProcessController, 
-    '/v1/selectionprocess',
-    '/v1/selectionprocess/<string:process_status>',
-    '/v1/selectionprocess/<int:idclient>/<int:idjobposition>')
+    "/v1/selectionprocess",
+    "/v1/selectionprocess/<string:process_status>",
+    "/v1/selectionprocess/<int:idclient>/<int:idjobposition>")
 
 api.add_resource(SelectionProcessCandidateController, 
-    '/v1/selectionprocesscandidate',
-    '/v1/selectionprocesscandidate/<int:idclient>',
-    '/v1/selectionprocesscandidate/<int:idclient>/<int:idjobposition>')
+    "/v1/selectionprocesscandidate",
+    "/v1/selectionprocesscandidate/<int:idclient>",
+    "/v1/selectionprocesscandidate/<int:idclient>/<int:idjobposition>")
 
 api.add_resource(CandidateWithoutSelectionProcessController, 
-    '/v1/candidatewithoutselectionprocess')
+    "/v1/candidatewithoutselectionprocess")
 
 api.add_resource(ClientController, 
-    '/v1/clients/',
-    '/v1/clients/<int:uid>')
+    "/v1/clients/",
+    "/v1/clients/<int:uid>")
 
 api.add_resource(JobPositionController, 
-    '/v1/jobposition/',
-    '/v1/jobposition/<int:idclient>',
-    '/v1/jobposition/<int:idclient>/<int:idjobposition>')
+    "/v1/jobposition/",
+    "/v1/jobposition/<int:idclient>",
+    "/v1/jobposition/<int:idclient>/<int:idjobposition>")
 
 api.add_resource(JobPositionCandidateController, 
-    '/v1/jobpositioncandidate',
-    '/v1/jobpositioncandidate/<int:idclient>/<int:idjobposition>',
-    '/v1/jobpositioncandidate/<int:idclient>/<int:idjobposition>/<int:idcandidate>')
+    "/v1/jobpositioncandidate",
+    "/v1/jobpositioncandidate/<int:idclient>/<int:idjobposition>",
+    "/v1/jobpositioncandidate/<int:idclient>/<int:idjobposition>/<int:idcandidate>")
 
 api.add_resource(CandidateResetTestController, 
-    '/v1/candidate/resettest')
+    "/v1/candidate/resettest")
 
 # Completar valores para formulario de registro de candidato
 api.add_resource(UbigeoController, 
-    '/v1/candidateform/ubigeo',
-    '/v1/candidateform/ubigeo/<int:idcountry>',
-    '/v1/candidateform/ubigeo/<int:idcountry>/<int:iddepartamento>',
-    '/v1/candidateform/ubigeo/<int:idcountry>/<int:iddepartamento>/<int:idprovincia>')
+    "/v1/candidateform/ubigeo",
+    "/v1/candidateform/ubigeo/<int:idcountry>",
+    "/v1/candidateform/ubigeo/<int:idcountry>/<int:iddepartamento>",
+    "/v1/candidateform/ubigeo/<int:idcountry>/<int:iddepartamento>/<int:idprovincia>")
 
 api.add_resource(SexoController, 
-    '/v1/candidateform/sexo')
+    "/v1/candidateform/sexo")
 
 api.add_resource(DocumentoIdentidadController, 
-    '/v1/candidateform/documentoidentidad')
+    "/v1/candidateform/documentoidentidad")
 
 api.add_resource(EstadoCivilController, 
-    '/v1/candidateform/estadocivil')
+    "/v1/candidateform/estadocivil")
 
 api.add_resource(TipoDireccionController, 
-    '/v1/candidateform/tipodireccion')
+    "/v1/candidateform/tipodireccion")
 
 api.add_resource(PsychologicalTestController, 
-    '/v1/candidateform/testpsicologicos')
+    "/v1/candidateform/testpsicologicos")
 
 # Menu reclutador
 api.add_resource(PsychologicalTestsController,
-    '/v1/candidateform/testpsicologicos/info/email=<string:email>')
+    "/v1/candidateform/testpsicologicos/info/email=<string:email>")
 
 # Obtener interpretación de las pruebas psicológicas
 api.add_resource(PsychologicalTestInterpretacionController, 
-    '/testpsicologico/interpretacion/candidato/<int:idcandidato>',
-    '/testpsicologico/download/informe/uid=<int:uid>&email=<string:email>&token=<string:token>')
+    "/testpsicologico/interpretacion/candidato/<int:idcandidato>",
+    "/testpsicologico/download/informe/uid=<int:uid>&email=<string:email>&token=<string:token>")
 
 api.add_resource(CandidatoSoporteTecnicoNotificacionController,
-    '/v1/candidato_soportetecnico_notificar')
+    "/v1/candidato_soportetecnico_notificar")
 
 api.add_resource(CandidatoEmailValidarController,
-    '/candidato_email_validar')
+    "/candidato_email_validar")
 
 """
 Eliminar luego de migrar código fuente.
 """
 api.add_resource(ReclutadorIdentificadorValidarController,
-    '/reclutador_identificador_validar')
+    "/reclutador_identificador_validar")
 
 api.add_resource(ReclutadorEmailValidarController,
-    '/reclutador_email_validar')
+    "/reclutador_email_validar")
 
 # Administrador accesos gestionar
 api.add_resource(UsuariosController, 
-    '/usuarios/',
-    '/usuarios/<int:uid>')
+    "/usuarios/",
+    "/usuarios/<int:uid>")
 
 api.add_resource(PerfilesController, 
-    '/perfiles/',
-    '/perfiles/<int:uid>')
+    "/perfiles/",
+    "/perfiles/<int:uid>")
