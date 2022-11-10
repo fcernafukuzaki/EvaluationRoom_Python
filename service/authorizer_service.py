cf52c2a005cafa9722d017bc2c5ef1a4a61714b7
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2022-11-09 21:04:19 -0500

    Feature/reclutador identificador validar (#22)
    * Commit
    
    * Hotfix
    
    * Update authorizer_service.py

--- service/authorizer_service.py
+++ service/authorizer_service.py
@@ -1,33 +1,41 @@
-import json
-from common.util import invoke_api
+from configs.flask_config import db
+from objects.login_user import LoginUser
+from service.recruiter.reclutadoridentificadorvalidar_service import ReclutadorIdentificadorValidarService
+from objects.usuario import Usuario, UsuarioSchema
 
+reclutadoridentificadorvalidar_service = ReclutadorIdentificadorValidarService()
+usuario_schema = UsuarioSchema()
 
 class AuthorizerService():
 
     def validate_recruiter_identify(self, token, email):
         if email and token:
-            url = 'https://evaluationroom-recruteridntval.herokuapp.com/reclutador_identificador_validar'
-            data = {"Authorization": token, "correoelectronico": email}
-            encoded_data = json.dumps(data).encode('utf-8')
-            response = invoke_api(url, body=encoded_data, method='POST')
-            print('Resultado de API: {} {}'.format(response.status, response.data))
-            if response.status == 200:
-                return True, 'Usuario valido', response.status, json.loads(response.data.decode('utf-8'))['idusuario']
-            return False, json.loads(response.data.decode('utf-8'))['mensaje'], response.status, None
+            email_valido, mensaje, usuario = reclutadoridentificadorvalidar_service.valida_email_reclutador(email)
+            if email_valido:
+                valido = self.validar_token_recruiter(token, usuario.idusuario)
+                if valido:
+                    return True, 'Usuario valido', 200, usuario.idusuario
+                return False, 'Operación no valida.', 403, None
+            return False, mensaje, 404, None
         return False, 'Usuario no valido.', 404, None
     
     def validate_recruiter_active(self, token, email):
         if email and token:
-            url = 'https://evaluationroom-recruteridntval.herokuapp.com/reclutador_email_validar'
-            data = {"Authorization": token, "correoelectronico": email}
-            encoded_data = json.dumps(data).encode('utf-8')
-            response = invoke_api(url, body=encoded_data, method='POST')
-            print('Resultado de API: {} {}'.format(response.status, response.data))
-            if response.status == 200:
-                return True, 'Usuario valido', response.status, json.loads(response.data.decode('utf-8'))
-            return False, json.loads(response.data.decode('utf-8'))['mensaje'], response.status, None
+            email_valido, mensaje, usuario = reclutadoridentificadorvalidar_service.valida_email_reclutador(email)
+            if email_valido:
+                return True, 'Usuario valido', 200, usuario_schema.dump(usuario)
+            return False, mensaje, 404, None
         return False, 'Usuario no valido.', 404, None
     
+    def validar_token_recruiter(self, hash, idusuario):
+        if hash:
+            return db.session.query(LoginUser
+                ).filter(LoginUser.hash == hash,
+                         LoginUser.date_logout == None,
+                         LoginUser.iduser == idusuario
+                         ).first()
+        return False
+
     def validate_token(self, token):
         if token:
             return True
