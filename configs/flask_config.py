bcc572b73363e68eef198393ee5170994e6a62aa
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2022-12-06 11:37:38 -0500

    Merge de otros repositorios (#26)
    * Feature/soporte tecnico (#19)
    
    * Feature/candidato email validar (#20)
    
    * Feature/reclutador identificador validar (#22)
    
    * Commit
    
    * Hotfix
    
    * Update authorizer_service.py
    
    * feature/administrador-accesos-gestionar
    
    * Format

--- configs/flask_config.py
+++ configs/flask_config.py
@@ -6,12 +6,12 @@ from os import environ
 
 from flask_cors import CORS
 
-DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-DATABASE_URI = DATABASE_URI[1:len(DATABASE_URI)-1]
+DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+DATABASE_URI = DATABASE_URI[1 : len(DATABASE_URI) - 1]
 
 # Aplicación de servidor
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
