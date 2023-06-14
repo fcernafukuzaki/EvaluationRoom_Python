4ed4297cd52b1c90acce462fea6cd7473ea3106c
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2023-06-14 14:15:49 -0500

    Update flask_config.py

--- configs/flask_config.py
+++ configs/flask_config.py
@@ -1,9 +1,9 @@
-from flask_cors import CORS
+from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api
-from os import environ
+from flask_cors import CORS, cross_origin
 
 
 DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
@@ -12,11 +12,11 @@ DATABASE_URI = DATABASE_URI[1 : len(DATABASE_URI) - 1]
 # Aplicación de servidor
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-
+db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
 
 CORS(app)
 
-with app.app_context():
-    db = SQLAlchemy(app)
+# with app.app_context():
+#     db = SQLAlchemy(app)
