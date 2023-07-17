3944c295962a9c40490fa4a1097639a72860b110
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2023-07-17 03:30:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from configs.routes import api_add_resource
 
 app = Flask(__name__)
@@ -7,6 +8,8 @@ api = Api(app)
 
 # Importar la línea api.add_resource desde routes.py
 api_add_resource(api)
+
+CORS(app)
 
 if __name__ == '__main__':
     app.run()
