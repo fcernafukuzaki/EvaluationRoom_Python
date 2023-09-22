da19f8851e14b60cd6adab4351359f470ee72a81
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2023-09-22 16:48:04 -0500

    Hot fix CORS

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_restful import Api
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from configs.routes import api_add_resource
 
 app = Flask(__name__)
 api = Api(app)
 
-CORS(app, resorces={r'/*': {"origins": '*'}})
-
 # Importar la línea api.add_resource desde routes.py
 api_add_resource(api)
+
+CORS(app, resorces={r'/d/*': {"origins": '*'}})
 
 if __name__ == '__main__':
     app.run()
