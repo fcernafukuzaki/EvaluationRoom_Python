b183437c77e96fa8c01aa081954ef21a560fd75d
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2023-10-12 10:58:07 -0500

    Eliminar clases no utilizadas
    Eliminar clases no utilizadas luego de la migración. Se eliminaron algunas clases que ya fueron migradas.

--- controller/candidate_info_controller.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-from service.candidate_info_service import CandidateInfoService
-
-candidate_info_service = CandidateInfoService()
-
-class CandidateInfoSimpleController(Resource):
-    
-    def get(self, idcandidate=None):
-        return candidate_info_service.get_candidates(idcandidate)
