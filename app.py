790da7b72e9af452a8a8d1cd1a9cac9b0dda3336
Francisco Cerna Fukuzaki <fcerna.ai@gmail.com>
2026-02-18 11:55:38 -0500

    Fix CORS configuration in app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ api = Api(app)
 # Importar la línea api.add_resource desde routes.py
 api_add_resource(api)
 
-CORS(app, resorces={r'/d/*': {"origins": '*'}})
+#CORS(app, resorces={r'/d/*': {"origins": '*'}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == '__main__':
     app.run()
